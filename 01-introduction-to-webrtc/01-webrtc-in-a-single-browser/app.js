e7397078031343f9112bd735a29d250814e36677
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-09-13 23:48:59 +0530

    Serve css from public folder and install nodemon

--- 01-introduction-to-webrtc/01-webrtc-in-a-single-browser/app.js
+++ 01-introduction-to-webrtc/01-webrtc-in-a-single-browser/app.js
@@ -1,9 +1,19 @@
+// All imports
 const express = require("express");
+const path = require("path");
 
+// Execute the express function
 const app = express();
 
+/* 
+  Set the view engine to show dynamic content in html with "ejs" files
+  2nd argument "views" is the folder name which has html files
+*/
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+// Serving the "public" folder statically (Serve the files in the folder as if they were in the root folder)
+app.use(express.static(path.join(path.dirname(require.main.filename), "public")));
 
 app.use("/", (req, res, next) => {
   /* 
@@ -12,7 +22,7 @@ app.use("/", (req, res, next) => {
     */
   if (req.originalUrl.includes("favicon.ico")) {
     return res.status(204).end();
-  }
+  } 
   // Forward the incoming request to the next middleware
   next();
 });
@@ -21,4 +31,5 @@ app.get("/", (req, res, next) => {
     res.render("index");
 });
 
+// Start the server on localhost with port "3000"
 app.listen(3000);
