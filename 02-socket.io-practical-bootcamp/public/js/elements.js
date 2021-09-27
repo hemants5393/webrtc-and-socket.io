3abcdd482eb1342bcfa5755d7f518bae3036e8da
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-09-28 01:24:28 +0530

    SPBC-02-Creating Chat Box with JavaScript

---
+++ 02-socket.io-practical-bootcamp/public/js/elements.js
@@ -0,0 +1,34 @@
+const getChatBox = () => {
+  const { chatboxLabel, chatboxMessagesId, chatboxInputId, chatboxId } = data;
+
+  const chatboxContainer = document.createElement("div");
+  chatboxContainer.classList.add("chatbox_container");
+  chatboxContainer.setAttribute("id", chatboxId);
+  chatboxContainer.innerHTML = `
+    <div class="chatbox_label_container">
+        <p class="chatbox_label">${chatboxLabel}</p>
+    </div>
+    <div class="messages_container" id="${chatboxMessagesId}">
+        <div class="message_container">
+            <p class="message_paragraph">
+            <span class="message_author">Hemant: </span>
+            Hello World!
+            </p>
+        </div>
+    </div>
+    <div class="new_message_input_container">
+        <input
+            type="text"
+            class="new_message_input"
+            id="${chatboxInputId}"
+            placeholder="Type your message..."
+        />
+    </div>
+  `;
+
+  return chatboxContainer;
+};
+
+export default {
+  getChatBox,
+};
--- 02-socket.io-practical-bootcamp/public/js/ui.js
+++ 02-socket.io-practical-bootcamp/public/js/ui.js
@@ -1,4 +1,9 @@
 import store from "./store.js";
+import elements from "./elements.js";
+
+const chatboxMessagesId = "group-chat-chatbox";
+const chatboxInputId = "group-chat-messages";
+const chatboxId = "group-chat-input";
 
 const goToChatPage = () => {
   const introductionPage = document.querySelector(".introduction_page");
@@ -10,11 +15,23 @@ const goToChatPage = () => {
 
   const username = store.getUsername();
   updateUsername(username);
+  createGroupChatBox();
 };
 
 const updateUsername = (username) => {
   const usernameLabel = document.querySelector(".username_label");
   usernameLabel.innerHTML = username;
+};
+
+const createGroupChatBox = (username) => {
+  const data = {
+    chatboxLabel: "Group chat",
+    chatboxMessagesId,
+    chatboxInputId,
+    chatboxId,
+  };
+
+  const chatbox = elements.getChatBox(data);
 };
 
 export default {
