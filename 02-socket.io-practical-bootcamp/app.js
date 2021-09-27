4b3e895ddf7a25f5911b5d98828f3c46e2290208
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-09-27 20:53:52 +0530

    SPBC-01-Initial setup for socket.io practical bootcamp project

---
+++ 02-socket.io-practical-bootcamp/app.js
@@ -0,0 +1,38 @@
+// All imports
+const express = require("express");
+
+// Execute the express function
+const app = express();
+
+/* 
+  Set the view engine to show dynamic content in html with "ejs" files
+  2nd argument "views" is the folder name which has html files
+*/
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+// Serving the "public" folder statically (Serve the files in the folder as if they were in the root folder)
+app.use(express.static(__dirname + "/public"));
+
+app.use("/", (req, res, next) => {
+  /* 
+    To handle favicon request issue.
+    If we don't use this code then each request runs twice.
+  */
+  if (req.originalUrl.includes("favicon.ico")) {
+    return res.status(204).end();
+  }
+  // Forward the incoming request to the next middleware
+  next();
+});
+
+app.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+// Start the server on localhost with port "3000"
+const server = app.listen(3000);
+
+// Setup socket.io
+const io = require("./socket").init(server);
+io.on("connection", (socket) => {});
