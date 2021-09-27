330d795dd427f950a6cf7322d5f4abbcab99c1d0
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-09-28 00:09:16 +0530

    SPBC-02-Adding event listener for name input

--- 02-socket.io-practical-bootcamp/public/js/main.js
+++ 02-socket.io-practical-bootcamp/public/js/main.js
@@ -1,3 +1,10 @@
+import store from "./store.js";
+
+const nameInput = document.querySelector(".introduction_page_name_input");
+nameInput.addEventListener("keyup", (event) => {
+  store.setUsername(event.target.value);
+});
+
 // "http://localhost:3000/" is the server url to connect to
 const socket = io("http://localhost:3000/");
 
