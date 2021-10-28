b2e7bcafa4e63a3187bf7424463774f46dac66e8
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-10-29 00:42:32 +0530

    SAN-Added prime ng package

--- 03-socket.io-angular-nodejs/frontend-angular/src/app/app.module.ts
+++ 03-socket.io-angular-nodejs/frontend-angular/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +13,10 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
