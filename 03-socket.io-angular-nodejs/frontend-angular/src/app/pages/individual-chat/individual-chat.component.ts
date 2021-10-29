1386ae8b30ed88e0da16380d940af94f8a2c055c
DESKTOP-NICHS2L\heman <hemant.s5393@gmail.com>
2021-10-30 01:00:19 +0530

    SAN-Added basic ui code for individual chat component

--- 03-socket.io-angular-nodejs/frontend-angular/src/app/pages/individual-chat/individual-chat.component.ts
+++ 03-socket.io-angular-nodejs/frontend-angular/src/app/pages/individual-chat/individual-chat.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-individual-chat',
   templateUrl: './individual-chat.component.html',
-  styleUrls: ['./individual-chat.component.scss']
+  styleUrls: ['./individual-chat.component.scss'],
 })
 export class IndividualChatComponent implements OnInit {
-
-  constructor() { }
+  public users: object[] = [];
+  constructor() {}
 
   ngOnInit(): void {
+    this.users = [
+      { name: 'Hemant' },
+      { name: 'John' },
+      { name: 'Jessica' },
+      { name: 'Peter' },
+      { name: 'Emily' },
+      { name: 'Joanna' },
+      { name: 'Selena' },
+      { name: 'Ron' },
+      { name: 'Harry' },
+      { name: 'Frank' },
+      { name: 'Ross' },
+      { name: 'Kane' },
+      { name: 'Steve' },
+      { name: 'Glenn' },
+      { name: 'Rick' },
+      { name: 'Daryl' },
+      { name: 'Maggie' },
+      { name: 'Carl' },
+      { name: 'Judith' },
+    ];
   }
-
 }
--- 03-socket.io-angular-nodejs/frontend-angular/src/app/shared/shared/shared.module.ts
+++ 03-socket.io-angular-nodejs/frontend-angular/src/app/shared/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenubarModule } from 'primeng/menubar';
+import { OrderListModule } from 'primeng/orderlist';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MenubarModule],
+  exports: [MenubarModule, OrderListModule],
 })
 export class SharedModule {}
